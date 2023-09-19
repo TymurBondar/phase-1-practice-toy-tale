3405145f4c14cf9c92c5c76df3694980b026899b
Tymur Bondar <tymurbondar@outlook.com>
2023-09-18 21:36:36 -0400

    first deliverable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+const addToyForm = document.querySelector(".add-toy-form");
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +14,33 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+addToyForm.onsubmit = (e) => {
+  e.preventDefault();
+  console.log("submitted");
+};
+
+function listToys() {
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(toys => {
+    toys.forEach(toy => {
+      let newToy = document.createElement("div");
+      newToy.className = 'card';
+      let newImage = document.createElement("img");
+      newImage.src = toy.image;
+      newImage.className = "toy-avatar";
+      let likes = document.createElement("p");
+      likes.textContent = `${toy.likes} Likes`
+      let likeBtn = document.createElement("button")
+      likeBtn.className = "like-btn";
+      likeBtn.id = toy.id;
+      likeBtn.textContent = "Like ❤️";
+      newToy.textContent = toy.name;
+      newToy.append(newImage, likes, likeBtn);
+      document.querySelector("#toy-collection").append(newToy);
+    });
+  })
+};
+
+listToys();
